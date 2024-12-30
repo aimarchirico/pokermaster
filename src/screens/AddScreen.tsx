673af59d50677b84382788a8054ae4a81495eb30
Aimár A. Chirico <aimarac@stud.ntnu.no>
2024-12-31 00:26:30 +0100

    feat(add): add validation for difference being zero

--- src/screens/AddScreen.tsx
+++ src/screens/AddScreen.tsx
@@ -1,76 +1,97 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { PlayerAdd } from "../components/PlayerAdd";
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Alert } from "react-native";
+import PlayerAdd from "../components/PlayerAdd";
 import { usePlayers } from "../contexts/PlayersContext";
-import { useGoogleSheets } from '../hooks/GoogleSheets'
-import { useAuth } from "../contexts/AuthContext";
-import { useTheme } from "@react-navigation/native";
+import { AmountData } from "../types/PlayerTypes";
+import useGoogleSheets from '../hooks/GoogleSheets'
+import { useStyles } from "../styles/StylesContext";
+import CustomAlert from "../components/CustomAlert";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import RootTabParamList from "../types/RootTabParamList";
 
 export const AddScreen = () => {
 
   const { players, setPlayers } = usePlayers();
-  const [inputAmounts, setInputAmounts] = useState<{ [key: string]: string }>({});
+  const [inputAmounts, setInputAmounts] = useState<{ [key: string]: AmountData }>({});
   const [resetTrigger, setResetTrigger] = useState(0);
   const { appendData } = useGoogleSheets();
-  const { auth } = useAuth();
-  const { colors } = useTheme();
+  const { globalStyles } = useStyles();
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const navigation = useNavigation<NavigationProp<RootTabParamList>>();
 
-  const handleAmountChange = (name: string, amount: string) => {
+  const handleAmountChange = (type: string, name: string, amount: number) => {
     setInputAmounts(prev => ({
       ...prev,
-      [name]: amount
+      [name]: {
+        ...prev[name],
+        [type]: amount
+      }
     }));
   };
 
   const handleConfirm = () => {
     try {
-      const data = players.map(player => inputAmounts[player.name] || "0");
-      appendData(data)
+      const data = players.map(player => 
+        (inputAmounts[player.name]?.end || 0) - (inputAmounts[player.name]?.start || 0));
+      const sum = data.reduce((a,b) => a+b,0);
+      if (sum !== 0)  {
+        throw Error(`Sum equals ${sum}. Please ensure sum equals zero.`)
+      }
+      appendData(data.map(value => value.toString()))
       const updatedPlayers = [...players];
       updatedPlayers.forEach(player => {
-        player.updateBalance(parseFloat(inputAmounts[player.name]) || 0);
+        player.updateBalance((inputAmounts[player.name]?.end || 0 - inputAmounts[player.name]?.start || 0));
       });
       setPlayers(updatedPlayers);
       setInputAmounts({})
       setResetTrigger(prev => prev + 1)
+      navigation.navigate('Total');
     }
     catch (error) {
-      console.error(error)
+      setAlertMessage(error.message);
+      setAlertVisible(true);
     }
     
   };
 
   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
+    subheaderCard: {
+      height: 50,
+      paddingRight: 40
     },
-    button: {
-      backgroundColor: colors.card,
-      padding: 10,
-      marginVertical: 5,
-      borderRadius: 5
-    },
-    text: {
-      color: colors.text
-    },
-  });
+  })
+  
 
   return (
-    <View style={styles.container}>
+    <View style={StyleSheet.compose(globalStyles.container,globalStyles.flexContainer)}>
+      <View style={StyleSheet.compose(globalStyles.card,styles.subheaderCard)}>
+        <Text style={StyleSheet.compose(globalStyles.text,globalStyles.subheader)}>Name</Text>
+        <Text style={StyleSheet.compose(globalStyles.text,globalStyles.rightSubheader)}>Start</Text>
+        <Text style={StyleSheet.compose(globalStyles.text,globalStyles.rightSubheader)}>End</Text>
+      </View>
+      
+    <ScrollView  contentContainerStyle={globalStyles.container}>
       {players?.map(player => (
         <PlayerAdd
           key={player.name}
           player={player}
-          onAmountChange={(amount) => handleAmountChange(player.name, amount)}
+          onAmountChange={(type,amount) => handleAmountChange(type, player.name, amount)}
           resetTrigger={resetTrigger}
         />
       ))}
-      <TouchableOpacity 
-        style={styles.button}
+
+    </ScrollView>
+    <TouchableOpacity 
+        style={globalStyles.card}
         onPress={handleConfirm}>
-        <Text style={styles.text}>Confirm changes</Text>
+        <Text style={StyleSheet.compose(globalStyles.text,globalStyles.buttonText)}>Save</Text>
       </TouchableOpacity>
+      <CustomAlert
+        visible={alertVisible}
+        message={alertMessage}
+        onClose={() => setAlertVisible(false)}
+      />
     </View>
   );
 };

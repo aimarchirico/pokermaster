21c20e582fb56ba74a1e2397a59b8bfe7e485eef
Aimár A. Chirico <aimarac@stud.ntnu.no>
2024-12-31 00:23:37 +0100

    style(total): use global styles for total component and screen

--- src/components/PlayerTotal.tsx
+++ src/components/PlayerTotal.tsx
@@ -2,24 +2,22 @@ import React, { View, Text, StyleSheet } from "react-native";
 import { PlayerTotalProps } from "../types/PlayerTypes";
 import { useStyles } from "../styles/StylesContext";
 
-export const PlayerTotal = ({ player }: PlayerTotalProps) => {
+const PlayerTotal = ({ player }: PlayerTotalProps) => {
   const { globalStyles } = useStyles();
+
+  const styles = StyleSheet.create({
+    balance: {
+      textAlign: 'right'
+    }
+  });
 
   return (
     <View style={globalStyles.card}>
-      <Text style={StyleSheet.compose(globalStyles.text, styles.name)}>{player.name}</Text>
+      <Text style={globalStyles.text}>{player.name}</Text>
       <Text style={StyleSheet.compose(globalStyles.text, styles.balance)}>{player.balance}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  name: {
-    fontSize: 20
-  },
-  balance: {
-    fontSize: 20,
-    color: '#fff',
-    textAlign: 'right'
-  },
-});
+export default PlayerTotal;
+

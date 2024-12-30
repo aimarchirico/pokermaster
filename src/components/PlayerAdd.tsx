421a049990f7d031e0757644b423b166335a61c8
Aimár A. Chirico <aimarac@stud.ntnu.no>
2024-12-31 00:22:09 +0100

    feat(add): add start and end amount for player add component

--- src/components/PlayerAdd.tsx
+++ src/components/PlayerAdd.tsx
@@ -4,52 +4,51 @@ import { PlayerAddProps } from "../types/PlayerTypes";
 import { useStyles } from "../styles/StylesContext";
 import { useTheme } from "@react-navigation/native";
 
-export const PlayerAdd = ({
+const PlayerAdd = ({
   player,
   onAmountChange,
   resetTrigger
 }: PlayerAddProps) => {
-  const [inputValue, setInputValue] = useState("0");
+  const [startValue, setStartValue] = useState("0");
+  const [endValue, setEndValue] = useState("0");
 
   useEffect(() => {
-    setInputValue("");
+    setStartValue("");
+    setEndValue("");
   }, [resetTrigger]);
 
-  const handleInputChange = (text: string) => {
-    setInputValue(text);
-    const amount = text || "0";
-    onAmountChange(amount);
+  const handleStartChange = (text: string) => {
+    setStartValue(text);
+    const amount = Number(text) || 0;
+    onAmountChange('start',amount);
+  };
+
+  const handleEndChange = (text: string) => {
+    setEndValue(text);
+    const amount = Number(text) || 0;
+    onAmountChange('end',amount);
   };
 
   const { globalStyles } = useStyles();
-  const { colors } = useTheme();
-
-  const styles = StyleSheet.create({
-    name: {
-      fontSize: 20
-    },
-    input: {
-      fontSize: 20,
-      borderColor: colors.border, 
-      borderWidth: 2,
-      borderRadius: 5,
-      padding: 10,
-      height: 40,
-      textAlign: 'right'
-    },
-  });
   
 
   return (
-    <ScrollView contentContainerStyle={globalStyles.card}>
-      <Text style={StyleSheet.compose(globalStyles.text, styles.name)}>{player.name}</Text>
+    <View style={globalStyles.card}>
+      <Text style={globalStyles.text}>{player.name}</Text>
       <TextInput
-        style={StyleSheet.compose(globalStyles.text, styles.input)}
-        value={inputValue}
-        onChangeText={handleInputChange}
+        style={StyleSheet.compose(globalStyles.text, globalStyles.input)}
+        value={startValue}
+        onChangeText={handleStartChange}
         keyboardType="numeric"
       />
-    </ScrollView>
+      <TextInput
+        style={StyleSheet.compose(globalStyles.text, globalStyles.input)}
+        value={endValue}
+        onChangeText={handleEndChange}
+        keyboardType="numeric"
+      />
+    </View>
   );
 };
 
+export default PlayerAdd;
